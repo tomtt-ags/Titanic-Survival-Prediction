1eb47f7c601952ea73145bf81987988cd037880c
Tom Thomas <tom1g2p3@outlook.com>
2025-09-08 23:05:32 +0100

    First 5

--- titanic.py
+++ titanic.py
@@ -3,3 +3,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style="whitegrid")
+titanic_ds = pd.read_csv('Titanic_Survival_Dataset/Datasets/Titanic-Dataset.csv')
+print(titanic_ds.head())
